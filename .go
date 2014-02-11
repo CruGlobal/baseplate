1d52baaf7ac041c9af623e8219e6b70104a3c112
thejamesdempsey <hello@thejamesdempsey.com>
2014-02-11 09:59:35 -0500

    .go

--- .go
+++ .go
@@ -2,14 +2,10 @@
 
 echo "Installing clean project"
 
-mv .git/modules .                                                                 && \
 rm -rf .git/                                                                      && \
 git init                                                                          && \
-mv modules .git/                                                                  && \
 git clone git@github.com:CruWeb/crubrand.git dev/styles/crubrand                  && \
-rm -rf dev/styles/crubrand/.git/                                                  && \
 git clone git@github.com:thejamesdempsey/brackets.css.git dev/styles/brackets.css && \
-rm -rf dev/styles/brackets.css/.git/                                              && \
 git add dev/styles/brackets.css                                                   && \
 git add dev/styles/crubrand                                                       && \
 git add .gitmodules                                                               && \
