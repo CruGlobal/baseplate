7178677726e6b227fc2a66c1b083cf67cf17ede3
thejamesdempsey <hello@thejamesdempsey.com>
2014-02-11 09:53:45 -0500

    Removing submodules

--- .go
+++ .go
@@ -2,13 +2,17 @@
 
 echo "Installing clean project"
 
-mv .git/modules .                 && \
-rm -rf .git/                      && \
-git init                          && \
-mv modules .git/                  && \
-git add dev/styles/brackets.css   && \
-git add dev/styles/crubrand.css   && \
-git add .gitmodules               && \
+mv .git/modules .                                                                 && \
+rm -rf .git/                                                                      && \
+git init                                                                          && \
+mv modules .git/                                                                  && \
+git clone git@github.com:CruWeb/crubrand.git dev/styles/crubrand                  && \
+rm -rf dev/styles/crubrand/.git/                                                  && \
+git clone git@github.com:thejamesdempsey/brackets.css.git dev/styles/brackets.css && \
+rm -rf dev/styles/brackets.css/.git/                                              && \
+git add dev/styles/brackets.css                                                   && \
+git add dev/styles/crubrand                                                       && \
+git add .gitmodules                                                               && \
 rm go
 
 echo "Cru project successfully installed"
